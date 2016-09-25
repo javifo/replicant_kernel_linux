3b05b00561ab15a2a882ac0da9ecfb974710f318
Wolfgang Wiedmeyer <wolfgit@wiedmeyer.de>
2016-09-25 20:06:40 +0200

    power: supply: max17042_battery: use VF SOC register for capacity property
    The capacity property uses the RepSOC register to report the current state
    of charge. This register did not provide a reliable SOC value during my
    testing with the max17047 variant on a Galaxy S3 (Trats2/GT-I9300). The
    reported value did not change or even stayed 0 in some cases.
    However, the VF SOC register provided an accurate SOC value at all times.
    It uses the voltage fuel gauge to determine the SOC.
    
    Signed-off-by: Wolfgang Wiedmeyer <wolfgit@wiedmeyer.de>

--- drivers/power/supply/max17042_battery.c
+++ drivers/power/supply/max17042_battery.c
@@ -312,7 +312,7 @@ static int max17042_get_property(struct power_supply *psy,
 		val->intval = data * 625 / 8;
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
-		ret = regmap_read(map, MAX17042_RepSOC, &data);
+		ret = regmap_read(map, MAX17042_VFSOC, &data);
 		if (ret < 0)
 			return ret;
 
